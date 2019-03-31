6b9e8bf181d5f3dfeabea58ec66a29ce1efabb14
Deosrc <deosrc@g-de.co.uk>
2019-03-31 16:50:26 +0100

    Created base VM configuration.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Condement
+# Condement
+
+## Requirements
+
+* VirtualBox
+* Vagrant
+
+## Configuration
+
+```yaml
+cpus: 2
+memory_mb: 2048
+hostname: "condement"
+host_work_dir: "C:\\Users\\Dale"
+show_gui: true
+```
+
+## Setup Instructions
+
+1. Clone the repository or download the files
+2. Open a command prompt to the directory
+3. Run `vagrant up`
---
+++ puppet/base.pp
@@ -0,0 +1,34 @@
+#######################################
+# Create Developer account
+#######################################
+user { 'dev':
+  ensure     => present,
+  password   => '$6$dPp8BPqDC0O1NL$cMhWP7ddwpfZTQJ1hFy8zqhi3o71W1QvXFsYybl4Vsc2r4qN7twOCoxHqHmAs7OhE/wkgbk5AaQoBMq8qF41C/', # condement
+  managehome => true,
+  shell      => '/bin/bash',
+  groups     => ['adm', 'sudo']
+}
+
+#######################################
+# Install Desktop UI & useful standard
+# software
+#######################################
+package { 'ubuntu-desktop':
+  ensure          => present,
+  install_options => '--no-install-recommends'
+}
+
+#######################################
+# Install VirtualBox Guest Additions
+#######################################
+package { 'virtualbox-guest-dkms':
+  ensure => present
+}
+
+package { 'virtualbox-guest-utils':
+  ensure => present
+}
+
+package { 'virtualbox-guest-x11':
+  ensure => present
+}
