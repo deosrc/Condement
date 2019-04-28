03f08ab8a41f3825d9f7ebd3636e3c317fc986bd
Deosrc <deosrc@g-de.co.uk>
2019-04-28 23:41:42 +0100

    Added error handling to host folder setup script.

--- system_base/add_host_folder.sh
+++ system_base/add_host_folder.sh
@@ -10,4 +10,13 @@ HOST_FOLDER_NAME=$1
 TARGET_DIRECTORY=$2
 
 mkdir -p $TARGET_DIRECTORY
+if [ $? -ne 0 ]; then
+    echo "Error while creating directory '$TARGET_DIRECTORY'." 1&>2
+    exit 1
+fi
+
 echo "$HOST_FOLDER_NAME    $TARGET_DIRECTORY    vboxsf    defaults    0    0" >> /etc/fstab
+if [ $? -ne 0 ]; then
+    echo "Error while adding '$HOST_FOLDER_NAME' host folder to '/etc/fstab'." 1&>2
+    exit 1
+fi
