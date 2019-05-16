9ff4c5d389dcda7bfcb3e3b2cfc507e9fe1162ea
Deosrc <deosrc@g-de.co.uk>
2019-05-16 22:09:31 +0100

    Added persistent storage for firefox.

--- software/firefox.pp
+++ software/firefox.pp
@@ -5,3 +5,22 @@
 package { 'firefox':
   ensure => present
 }
+
+# Configure folders for persistent storage
+file { '/mnt/persistent_storage/firefox':
+  ensure => directory,
+  owner  => 'dev',
+  group  => 'dev'
+}
+
+file { '/home/dev/.mozilla':
+  ensure => directory,
+  owner  => 'dev',
+  group  => 'dev'
+}
+
+file { '/home/dev/.mozilla/firefox':
+  ensure  => link,
+  target  => '/mnt/persistent_storage/firefox',
+  require => File['/mnt/persistent_storage/firefox', '/home/dev/.mozilla']
+}
